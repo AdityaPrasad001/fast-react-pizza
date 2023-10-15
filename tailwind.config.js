3c7a590a1566bc2139a7759fa1ec06a6ab944cd9
AdityaPrasad001 <108456699+AdityaPrasad001@users.noreply.github.com>
2023-10-15 17:24:06 +0530

    #06: Added Styles with tailwind css
    Added Styles with tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      // we can override the sans, which is a default for tailwind
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
